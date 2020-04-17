7be4a21037661011cf2d221d880867b51d2bfe93
Ben Coughlan <ben.coughlan@cgsy.com.au>
2020-04-18 09:27:41 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name='FSM',
+	version='0.1',
+	author='Ben Coughlan',
+	author_email='ben@cgsy.com.au',
+	packages=find_packages(),
+	license_file='LICENSE',
+)
